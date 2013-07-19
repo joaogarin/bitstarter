f70844db6bbc7b46b6cdd83ed04a5359474223b8
Ubuntu <ubuntu@ip-172-31-25-85.us-west-2.compute.internal>
2013-07-19 15:46:01 +0000

    Alterado web.js para o port 8080

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var content = fs.readFileSync('index.html', 'utf8');
  response.send(content);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
